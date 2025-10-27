3df7b7f0b453c40083be43d350ed3e785cd40dac
liviu <liviu.traub04@gmail.com>
2025-10-27 17:44:37 +0200

    started developing auth service

---
+++ AuthenticationService/src/main/java/org/example/authenticationservice/entity/UserEntity.java
@@ -0,0 +1,14 @@
+package org.example.authenticationservice.entity;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Builder
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+
+
+}
